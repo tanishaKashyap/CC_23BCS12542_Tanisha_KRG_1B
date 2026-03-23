216648e20f3c7d1edc1513ed3849eab098476777
tanishaKashyap <tanisha.pankajj@gmail.com>
2026-03-23 11:43:25 +0530

    EXP-6 DONE

--- EXPERIMENT-6/main.cpp
+++ EXPERIMENT-6/main.cpp
@@ -1,69 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> bfstopoSort(int V, vector<int> adj[]){
-    vector<int> indegree(V, 0);
-    queue<int> q;
-    vector<int> ans;
-    for(int i = 0; i < V; i++){
-        for(auto a : adj[i]){
-            indegree[a]++;
-        }
+vector<int> topo(int V, vector<int> adj[]){
+    vector<int> in(V,0),ans;
+    for(int i=0;i<V;i++){
+        for(auto v:adj[i]) in[v]++;
     }
-    for(int i = 0; i < V; i++){
-        if(indegree[i] == 0){
-            q.push(i);
-        }
+    queue<int> q;
+    for(int i=0;i<V;i++){
+        if(in[i]==0) q.push(i);
     }
     while(!q.empty()){
-        int qNode = q.front();
+        int u=q.front();
         q.pop();
-        ans.push_back(qNode);
-        for(auto a : adj[qNode]){
-            indegree[a]--;
-            if(indegree[a] == 0){
-                q.push(a);
-            }
+        ans.push_back(u);
+        for(auto v:adj[u]){
+            in[v]--;
+            if(in[v]==0) q.push(v);
         }
     }
     return ans;
 }
 
 int main(){
-    vector<int> adj[26];
-    set<pair<int,int>> edges;
-    int N;
-    cin >> N;
-    vector<string> words(N);
-    for(int i = 0; i < N; i++) cin >> words[i];
+    int n;
+    cin>>n;
+    vector<string> w(n);
+    for(int i=0;i<n;i++) cin>>w[i];
 
-    for(int i = 0; i < N-1; i++){
-        int len = min(words[i].size(), words[i+1].size());
-        bool found = false;
-        for(int j = 0; j < len; j++){
-            if(words[i][j] != words[i+1][j]){
-                int u = words[i][j] - 'a';
-                int v = words[i+1][j] - 'a';
-                if(!edges.count({u, v})){
-                    adj[u].push_back(v);
-                    edges.insert({u, v});
-                }
-                found = true;
+    vector<int> adj[26];
+
+    for(int i=0;i<n-1;i++){
+        int len=min(w[i].size(),w[i+1].size());
+        bool f=false;
+        for(int j=0;j<len;j++){
+            if(w[i][j]!=w[i+1][j]){
+                adj[w[i][j]-'a'].push_back(w[i+1][j]-'a');
+                f=true;
                 break;
             }
         }
-        if(!found && words[i].size() > words[i+1].size()){
-            cout << "Impossible";
+        if(!f && w[i].size()>w[i+1].size()){
+            cout<<"Impossible";
             return 0;
         }
     }
 
-    int V = 26;
-    vector<int> ans = bfstopoSort(V, adj);
-    if((int)ans.size() != V){
-        cout << "Impossible";
+    vector<int> res=topo(26,adj);
+
+    if(res.size()!=26){
+        cout<<"Impossible";
         return 0;
     }
-    for(auto it : ans) cout << char(it + 'a');
+
+    for(auto x:res) cout<<char(x+'a');
     return 0;
 }
