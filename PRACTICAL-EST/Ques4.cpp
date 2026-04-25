2929911405d36040eca2968d6504d4e905df3a7c
tanishaKashyap <tanisha.pankajj@gmail.com>
2026-04-25 10:39:17 +0530

    PRACTICAL-EST DONE

--- PRACTICAL-EST/Ques4.cpp
+++ PRACTICAL-EST/Ques4.cpp
@@ -4,20 +4,18 @@ using namespace std;
 vector<int> topoSort(int V, vector<vector<int>>& edges) {
     vector<vector<int>> adj(V);
     vector<int> indegree(V, 0);
-    // Build graph
     for(auto &e : edges) {
         adj[e[0]].push_back(e[1]);
         indegree[e[1]]++;
     }
     queue<int> q;
-    // Push nodes with indegree 0
+
     for(int i = 0; i < V; i++) {
         if(indegree[i] == 0)
             q.push(i);
     }
     vector<int> ans;
 
-    // BFS
     while(!q.empty()) {
         int node = q.front(); q.pop();
         ans.push_back(node);
